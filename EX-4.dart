6515ceba9a7e9c48f22f054a9131cdd39a0c93d9
sutheavy22 <sutheavy22@gmail.com>
2025-10-22 19:40:21 +0700

    Change

--- EX-4.dart
+++ EX-4.dart
@@ -6,7 +6,6 @@ class Product {
 
   Product({required this.name, required this.price});
 }
-
 class Customer {
   final String name;
   final String address;
@@ -24,12 +23,59 @@ class OrderItem {
     return product.price * quantity;
   }
 }
-
 class Order {
   final int id;
   final Customer customer;
   final List<OrderItem> items;
   final DeliveryType deliveryType;
 
-  Order({required this.id,required this.customer,required this.items, required this.deliveryType,});
+  Order({
+    required this.id,
+    required this.customer,
+    required this.items,
+    required this.deliveryType,
+  });
+
+   double getTotalAmount() {
+    double total = 0;
+    for (var item in items) {
+      total += item.getTotal();
+    }
+
+    if (deliveryType == DeliveryType.delivery) {
+      total += 0; 
+    }
+
+    return total;
+  }
+
+  void printSummary() {
+    print("Order $id for ${customer.name}");
+    print("Delivery Type: ${deliveryType.name}");
+    for (var item in items) {
+      print("${item.product.name} x${item.quantity} = \$${item.getTotal()}");
+    }
+    print("Total Amount: \$${getTotalAmount()}");
+  }
+}
+
+void main() {
+  var product1 = Product(name: "Laptop", price: 120);
+  var product2 = Product(name: "Mouse", price: 2.5);
+
+
+  var customer = Customer(name: "Theavy", address: "Phnom Penh");
+
+
+  var item1 = OrderItem(product: product1, quantity: 3);
+  var item2 = OrderItem(product: product2, quantity: 2);
+
+  var order1 = Order(
+    id: 01,
+    customer: customer,
+    items: [item1, item2],
+    deliveryType: DeliveryType.delivery,
+  );
+
+  order1.printSummary();
 }
